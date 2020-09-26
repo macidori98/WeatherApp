0ba3092b692763d54a1910504eaec6f27c31916f
Dorottya Szabó <macidori98@gmail.com>
2020-09-26 19:52:09 +0300

    inotifypropertychanged implemented

---
+++ WeatherApp/ViewModel/Helpers/AccuWeatherHelper.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using WeatherApp.Model;
+
+namespace WeatherApp.ViewModel.Helpers
+{
+  public class AccuWeatherHelper
+  {
+    public const string BASE_URL = "http://dataservice.accuweather.com/";
+    public const string AUTOCOMPLETE_ENDPOINT = "locations/v1/cities/autocomplete?apikey={0}&q={1}";
+    public const string API_KEY = "RLtOdPUgeoGtSLAZ3633S2CDPSDy9JSy";
+    public const string CURRENT_CONDITION_ENDPOINT = "currentconditions/v1/{0}?apikey={1}";
+
+    public static async Task<List<City>> GetCities(string query)
+    {
+      List<City> cities = new List<City>();
+
+      string url = BASE_URL + string.Format(AUTOCOMPLETE_ENDPOINT, API_KEY, query);
+
+      using (HttpClient client = new HttpClient())
+      {
+        var resp = await client.GetAsync(url);
+        string json = await resp.Content.ReadAsStringAsync();
+
+        cities = JsonConvert.DeserializeObject<List<City>>(json);
+      };
+
+      return cities;
+    }
+
+    public static async Task<CurrentCondition> GetCurrentConditions(string cityKey)
+    {
+      CurrentCondition currentCondition = new CurrentCondition();
+
+      string url = BASE_URL + string.Format(CURRENT_CONDITION_ENDPOINT, cityKey, API_KEY);
+
+      using (HttpClient client = new HttpClient())
+      {
+        var resp = await client.GetAsync(url);
+        string json = await resp.Content.ReadAsStringAsync();
+
+        currentCondition = JsonConvert.DeserializeObject<List<CurrentCondition>>(json).FirstOrDefault();
+      };
+
+      return currentCondition;
+    }
+  }
+}
---
+++ WeatherApp/ViewModel/WeatherVM.cs
@@ -0,0 +1,76 @@
+﻿using System.ComponentModel;
+
+using WeatherApp.Model;
+
+namespace WeatherApp.ViewModel
+{
+  public class WeatherVM : INotifyPropertyChanged
+  {
+    public WeatherVM()
+    {
+      //csak akkor jelennek meg az ertekek amikor design modban vagyunk
+      if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+      {
+        this.SelectedCity = new City()
+        {
+          LocalizedName = "New York"
+        };
+
+        this.CurrentCondition = new CurrentCondition()
+        {
+          WeatherText = "Partly cloudy",
+          Temperature = new Temperature()
+          {
+            Metric = new Units()
+            {
+              Value = 21
+            }
+          }
+        };
+      }
+    }
+
+    private string query;
+
+    public string Query
+    {
+      get { return this.query; }
+      set
+      {
+        this.query = value;
+        this.OnPropertyChanged("Query");
+      }
+    }
+
+    private CurrentCondition currentCondition;
+
+    public CurrentCondition CurrentCondition
+    {
+      get { return this.currentCondition; }
+      set
+      {
+        this.currentCondition = value;
+        this.OnPropertyChanged("CurrentCondition");
+      }
+    }
+
+    private City selectedCity;
+
+    public City SelectedCity
+    {
+      get { return this.selectedCity; }
+      set
+      {
+        this.selectedCity = value;
+        this.OnPropertyChanged("SelectedCity");
+      }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private void OnPropertyChanged(string propertyName) // trigerring the event
+    {
+      this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+  }
+}
